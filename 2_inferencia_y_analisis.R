a7533ee6745e0ee6002743e823cf1ac68f6e95b9
juancamiloespana <camilo1260@gmail.com>
2022-06-08 11:24:20 -0500

    ejemplo completo regresion

--- 2_inferencia_y_analisis.R
+++ 2_inferencia_y_analisis.R
@@ -17,6 +17,26 @@ modelo=lm(formula=resistencia~porcentaje,data=datos)
 #### Ver resumen
 
 summary(modelo)
-
 mean(datos$resistencia)
+
+
+#### calcular I.C para coeficientes
+
+confint(object=modelo,level=.95)
+
+##Intervalo de confianza para datos con los que se entreno
+predict(object=modelo,interval="confidence")
+
+####generar datos nuevos
+datos_resistencia2<-data.frame("porcentaje"=runif(5,max=35,min=24))
+
+
+####Intervalo de confianza datos nuevos ####
+
+predict(object=modelo, newdata = datos_resistencia2,interval="prediction")
+
+
+
+library(tidyverse)
+data(Orange)
 
