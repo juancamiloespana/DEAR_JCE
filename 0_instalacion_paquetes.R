4faa019107e8e5c1c2d5bf9cb7693df841cb1479
juancamiloespana <camilo1260@gmail.com>
2022-06-10 09:52:05 -0500

    clase supuestos

--- 0_instalacion_paquetes.R
+++ 0_instalacion_paquetes.R
@@ -1,1 +1,10 @@
 install.packages("nortest") # Validación de supuestos
+
+
+#####instalar paquetes para validación de supuestos
+
+install.packages("car")
+install.packages("tseries")
+install.packages("nortest")
+install.packages("goftest")
+install.packages("lmtest")
