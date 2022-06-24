87c7fac4a2ad897aec57d8fd71c559f4b4af6a0f
juancamiloespana <camilo1260@gmail.com>
2022-06-24 12:35:19 -0500

    incumplimiento supuestos

--- 4_1_Quiz3.R
+++ 4_1_Quiz3.R
@@ -9,6 +9,9 @@ library(goftest)
 library(lmtest)
 
 
+library(datasets)
+
+
 set.seed(1)
 r1=runif(1,min=10, max=80)
 r2=runif(1,min=5, max=30)
@@ -20,25 +23,37 @@ data(iris)
 a<-iris$Sepal.Length*r1
 b<-iris$Petal.Length*r2
 
-mean(a)
+
+datos1=data.frame("n_defectos_promedio"=b, "n_productos_devueltos"=a)
 
 
-datos1=data.frame("presupuesto_mercadeo"=b, "ventas_totales"=a)
 
 
-mod=lm(ventas_totales ~presupuesto_mercadeo,data=datos1)
+
+mod=lm(n_productos_devueltos ~n_defectos_promedio,data=datos1)
 
 summary(mod)
-confint(mod,level=0.5)
+confint(mod,level=0.99)
 
 predict(mod,interval="confidence")
 
 res=mod$residuals
 
 goftest::ad.test(res, pnorm, 0, sd(res))
+
+jarque.bera.test(res)
+shapiro.test(res)
+
 bptest(mod)
 
 qqPlot(res)
 
 dwtest(mod)
 bgtest(mod)
+
+acf(res)
+
+
+
+
+
--- 5_0correciones_incumplimientos.R
+++ 5_0correciones_incumplimientos.R
@@ -15,15 +15,19 @@ library(lmtest)
         
       base_modelo=read.csv('https://raw.githubusercontent.com/juancamiloespana/DEAR_JCE/master/defectos.csv')
         
-        ###Graficar para identificar datos inusuales
-        y=base_modelo$defectos
-        x1=base_modelo$empleados_calidad
-  
+      ### Para simplificar nombre de variables
+      
+      y=base_modelo$defectos
+      x1=base_modelo$empleados_calidad
       names(base_modelo)=c("y","x1")  
       
-        plot(x1,y)
-        boxplot(y)
-        boxplot(x1)
+      ###Graficar para identificar datos inusuales
+
+      
+        
+        plot(base_modelo$x1,base_modelo$y)
+        boxplot(base_modelo$y)
+        boxplot(base_modelo$x1)
         
         ####Ajustar modelo y verificar desempeño ###
         mod=lm(y~x1,data=base_modelo)
@@ -38,6 +42,7 @@ library(lmtest)
         res=mod$residuals
         shapiro.test(res)
         jarque.bera.test(res)
+        
         bptest(mod)
         dwtest(mod)
         bgtest(mod)
@@ -53,7 +58,7 @@ library(lmtest)
         
         base_modelo=subset(base_modelo, x1<=lim_sup & x1>lim_inf)
         
-        
+        boxplot(base_modelo$x1)
         #### Verificar base despues de eliminar atípicos
         plot(base_modelo$x1, base_modelo$y)
         
@@ -63,6 +68,7 @@ library(lmtest)
         
         plot(base_modelo$x1,base_modelo$y)
         abline(mod)
+        boxplot(x1)
         
   ##### verificar supuestos del modelo 
         res=mod$residuals
@@ -77,54 +83,16 @@ library(lmtest)
         
     ##1.2  Efectos sobre el modelo #####
         ####Crear variables ficticia 
-        set.seed(200)
-        x1=runif(n=1000,min=20,max=80)
-        x1[901:1000]= runif(10,120,140)
-        e=rnorm(1000,0,5)
+          
+        datos=read.csv('https://raw.githubusercontent.com/juancamiloespana/DEAR_JCE/master/plantas.csv')
+        names(datos)=c("indice","y","x")
         
         
-        ####Agregar atípicos ficticios
-        y= 30 + 0.5*x1 + e
-        y[901:1000]= runif(10,150,200)
-        
-        
-        ####Crear data frame
-        base_modelo=data.frame(y,x1)
-        
-        #####Ajustar un modelo de regresión ###
-        plot(x1,y)
-        mod<-lm(y~x1)
-        
-        #### Verificar desempeño con R2 y desviación del modelo
-        
-        summary(mod)
-        
-        
-        ###Verificar graficamente datos inusuales
-        plot(x1,y)
+        plot(datos$x,datos$y) 
+        mod=lm(y~x,data=datos)
         abline(mod,col="red")
         
-        #### Aplicar función para detectar datos influyentes 
-        datos_influyentes<- influence.measures(mod)
-        
-    
-        #####Extraer filas de los datos influyentes 
-        
-        influyentes<-summary(datos_influyentes)
-        
-        filas_filtrar<-as.numeric(row.names(influyentes))
-        
-        
-        ####Base filtrada sin datos influyentes
-        
-        base_modelo2<-base_modelo[-filas_filtrar, ]
-        
-        
-        ##### Gráfica sin influyentes.
-        plot(base_modelo2$x1,base_modelo2$y)
-        
-        
-        ####Verificar supuestos
+        ###supuestos ###
         
         res=mod$residuals
         shapiro.test(res)
@@ -133,6 +101,25 @@ library(lmtest)
         dwtest(mod)
         bgtest(mod)
         
+        ########## Eliminación puntos de influencia
+        
+        medidas=influence.measures(mod)
+        solo_inf=summary(medidas)
+        filas_filtrar=as.numeric(row.names(solo_inf))
+        datos_f=datos[-filas_filtrar,]
+        
+        plot(datos_f$x,datos_f$y)
+        
+        mod2=lm(y~x,datos_f)
+        summary(mod2)
+        summary(mod)
+        
+        res2=mod2$residuals  
+        shapiro.test(res2)
+        jarque.bera.test(res2)
+        bptest(mod2)
+        dwtest(mod2)
+        bgtest(mod2)
 
 #2. Transformación de variable ####
         
@@ -140,106 +127,71 @@ library(lmtest)
   
     ###Crear base que incumple supuesto
     
-    x1=runif(1000,-80,80)
-    e=rnorm(1000,10,500)
+        ###Crear base que incumple supuesto
+        
+
+        
+        datos=read.csv('https://raw.githubusercontent.com/juancamiloespana/DEAR_JCE/master/flujo.csv')
+        names(datos)=c('ind','y','x')
     
-    y= 5+5*x1^2+ e
-    
-    
-    #### ver graficamente incumplimiento
-   
-    mod<-lm(y~x1)
-    plot(x1,y)
-    abline(mod)
-    
-    #### Verificar desempeño con R2 y desviación del modelo
-    summary(mod)
-    
-    
-    res=mod$residuals
-    shapiro.test(res)
-    jarque.bera.test(res)
-    bptest(mod)
-    dwtest(mod)
-    bgtest(mod)
-    
-    
-    
-    ###Transformar variable para cumplimiento
-  
-    xmod=x1^2
-    
-    ### Ajustar modelo con variable transformada
-    mod<-lm(y~xmod)
-    
-    #### Verificar desempeño con R2 y desviación del modelo
-    summary(mod)
-    
-    
-    res=mod$residuals
-    shapiro.test(res)
-    jarque.bera.test(res)
-    bptest(mod)
-    dwtest(mod)
-    bgtest(mod)
+        
+        datos$z=datos$x^2
+        mod1=lm(y~x,data=datos)
+        
+        plot(datos$x,datos$y)
+        abline(mod1,col="red")
+        
+        summary(mod1)
+        res1=mod1$residuals
+        shapiro.test(res1)
+        bptest(mod1)
+        dwtest(mod1)
+        
+
+        
+        plot(datos$z,datos$y)
+        
+        mod2=lm(y~z,data=datos)
+        abline(mod2,col="red")
+        summary(mod2)
+        
+        res2=mod2$residuals
+        shapiro.test(res2)
+        bptest(mod2)
+        dwtest(mod2)
+        
+        
+        
 
  
  ## 2.2transformación respuesta####
     
     ###Crear base que incumple supuesto
     
-    set.seed(000)
-    x1=runif(1000,0,560)
-    e=rnorm(1000,1850,1850)
-    y= ((200*x1+e)^(1/4))
+    datos=read.csv('https://raw.githubusercontent.com/juancamiloespana/DEAR_JCE/master/energia.csv')
+    
+        
+    names(datos)=c("i","y","x") 
+    plot(datos$x,datos$y)
+    mod=lm(y~x,data=datos)
+    
+    pt=powerTransform(mod)
+    summary(pt)
+    
+    datos$w=datos$y^4
+    
+    plot(datos$x,datos$w)
+    
+    mod=lm(w~x,data=datos)
+    abline(mod)
+    
+    res=mod$residuals
+    shapiro.test(res)
+    bptest(mod)
+    dwtest(mod)
     
     
-    
-    #### ver graficamente incumplimiento
   
-   
-    mod<-lm(y~x1)
-
-    plot(x1,y)
-    abline(mod)
-    #### Verificar desempeño con R2 y desviación del modelo
-    summary(mod)
-    
-    ######verificar supuestos ####
-    res=mod$residuals
-    shapiro.test(res)
-    jarque.bera.test(res)
-    bptest(mod)
-    dwtest(mod)
-    bgtest(mod)
-    
-    
-    
-    
-    ###Transformar variable para cumplimiento
-    
-    trans<- powerTransform(mod1)
-    summary(trans)
-    
-    ymod<-y^4
-    ### Ajustar modelo con variable transformada
-    mod<-lm(ymod~x1)
-    
-    par(mfrow=c(1,2))
-    
-    plot(x1,ymod)
-    abline(mod)
-
-    #### Verificar desempeño con R2 y desviación del modelo
-    summary(mod)
-    
-    res=mod$residuals
-    shapiro.test(res)
-    jarque.bera.test(res)
-    bptest(mod)
-    dwtest(mod)
-    bgtest(mod)
-
 
   
     
