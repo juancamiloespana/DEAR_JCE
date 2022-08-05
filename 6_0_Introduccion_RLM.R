4d73bc800c96f60b587a0be380b189b36d7359a8
juancamiloespana <camilo1260@gmail.com>
2022-08-05 10:09:32 -0500

    introducción multiple y desempeño

--- 6_0_Introduccion_RLM.R
+++ 6_0_Introduccion_RLM.R
@@ -7,24 +7,29 @@ library(plotly)  ###para gráficos Fancys
 ###############
 
 
-
-####### ##### Ajustar regresión lineal multiple
-
-### Cargar datos ####
-
-
-
 ##############################
 ### Ajustar modelo RLM###
 ###################################
 
 
+### Cargar datos microsoft
+url='https://raw.githubusercontent.com/juancamiloespana/DEAR_JCE/master/los_microsoft.csv'
+datos_losm=read.csv(url)
+
+### ajustar modelo con pulse y glucose
+
+mod=lm(data=datos_losm,lengthofstay~pulse+glucose)
+
+sum_rl=summary(mod)
 
 ######Ajsutar anova ####
 
+an=aov(mod)
+summary(an)
 
 #####extraer R2 y R2 ajustado
 
+sum_rl$adj.r.squared
 
 ################################################
 ### Variable cualitativa crear base ficticia  ####
@@ -32,14 +37,32 @@ library(plotly)  ###para gráficos Fancys
 
 ### crear edad como seq de 20 a 50
 ### crear variable antibiotico el 50% es no, el 20% es Bajo y el 30% es ALTO
-#### variable respuesta si toma antibiotico b0= 0b1 es 0,7 si no es -0,5 y bo es 50
+#### variable respuesta si no toma antibiotico b0= 0, b1 es 0,7 si sí es -0,5 y bo es 50
 ### error estándar para y
 ### crear data frame
+
+set.seed(123)
+edad=seq(20,50)
+antibiotico<-case_when(
+  runif(31,0,1)<=0.5 ~ "NO",
+  runif(31,0,1)<=0.7~"BAJO",
+  TRUE~"ALTO"
+  
+)
+table(antibiotico)
+
+y=case_when(antibiotico=="NO"~ 0.7*edad,
+            TRUE~ 50 -0.5*edad
+              )
+y=y+rnorm(31)
+
+datos=data.frame(y,edad,antibiotico)
 
 
 
 ####ajustar modelo y analizar resumen de cualitativa
-
+mod2=lm(data=datos,y~.)
+summary(mod2)
 
 
 ############################################
@@ -55,12 +78,32 @@ library(plotly)  ###para gráficos Fancys
 ### crear data frame
 
 
+set.seed(123)
+
+edad=seq(20,50)
+
+antibiotico<-case_when(
+  runif(31,0,1)<=0.5 ~ 0,
+  TRUE~1
+)
+
+table(antibiotico)
+
+y=case_when(antibiotico==0~ 0.7*edad,
+            TRUE~ 50 -0.5*edad
+)
+y=y+rnorm(31)
+
+datos=data.frame(y,edad,antibiotico)
+
+
 ################ Analizar gráficamente ############
 
 ###ver correlación entre variables explicativa
 
+plot_ly(data=datos, y=~y, x=~edad, color=~antibiotico)
 
-
+boxplot(edad~antibiotico)
 
 #####ver gráfico de interacción en plotly
 
@@ -68,8 +111,8 @@ library(plotly)  ###para gráficos Fancys
 
 ####ajustar modelo rl con interacción
 
-
-
+mod3=lm(data=datos, y~edad*antibiotico)
+summary(mod3)
 
 ##### analizar resume
 
@@ -97,20 +140,31 @@ library(plotly)  ###para gráficos Fancys
 ### y con b1 =3 y error normal estánar 
 
 
+x=runif(50,15,70)
+x2=x+rnorm(50,0,10)
+
+y=3*x +rnorm(50)
+
+datos=data.frame(y,x,x2)
+
 
 ###### Ajuste un modelo con sólo el gasto en calidad
 ###### Cuánto crece la utilidad si se aumento una unidad la inversion en calidad
 
+mod1=lm(data=datos,y~x2)
 
+summary(mod1)
 
 
 ### Agregue a ese modelo la variable publicidad
 ### ¿Qué pasa con la relación entre utilidad y calidad?
 
-
+mod2=lm(data=datos,y~x+x2)
+summary(mod2)
 ### Verifique la multicolinealidad.
 
 
+VIF(mod2)
 
 
 
