17cf0a645f5608c6083fd658ce8794ad939e60ab
juancamiloespana <camilo1260@gmail.com>
2023-02-16 14:44:18 -0500

    organizar tablas

---
+++ 001_pruebas_hipótesis.R
@@ -0,0 +1,21 @@
+
+mu=30 ### media poblacional y media de las medias muestrales por teorema central del limite
+sdm=3/sqrt(100) ### estimar la desviacion de xbarra (no es la misma desviacion que la variable original), 
+alpha=0.05 ### error tipo 1 limite para comparar valor P
+
+### Error tipo 1 (ortodoxo): Probabilidad de equivocarse si se rechaza Ho
+### Error tipo 1 (Nemotecnia): Probabilidad de que Ho sea verdadera
+
+### Valor p y alpha son el mismo tipo de error, alpha es el valor mas bajo que necesito del valor p para aprobar Ho
+
+
+val_crit=qnorm(p=alpha, mean= mu, sd=sdm, lower.tail = F) ## valor critico para alpha 0.05
+
+url='https://raw.githubusercontent.com/juancamiloespana/DEAR_JCE/master/data/muestras_inferencias.csv'
+
+muestra1= read.csv(url)  ## tabla con muestras
+
+xbarra=mean(muestra1$muestra1) ## calculo del estadistico de prueba
+
+val_p=pnorm(xbarra, mean=mu, sd=sdm, lower.tail = F) ### probabilidad de que con ese estadístico Ho se cumpla (probabilidad de equivocarse si rechaza Ho dado ese esttadistico) 
+
