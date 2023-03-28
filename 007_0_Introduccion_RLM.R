e716245e33a26f6567fa588cd2ae88faf5283da9
juancamiloespana <camilo1260@gmail.com>
2023-03-28 12:55:17 -0500

    introduccion RLM

---
+++ 007_0_Introduccion_RLM.R
@@ -0,0 +1,77 @@
+#####CArgar paquetes #############3
+
+library(regclass) ### Para calcular VIF
+library(dplyr)  ### para case_when
+library(plotly)  ###para gráficos Fancys
+
+
+
+################################################
+### Variable cualitativa  ####
+################################################
+
+####datos 'los_cual.csv'
+### predecir los(y) con base en edad y antibitoicos
+#####
+
+
+
+############################################
+################Interaccion ##############
+##########################################
+
+###datos 'los_intera.csv'
+###predecir los(y) con base en edad y antibioticos
+####
+
+################ Analizar gráficamente ############
+
+###ver correlación entre variables explicativas
+
+### ver grafico de interaccion
+
+
+
+####ajustar modelo rl con interacción
+
+
+
+##### analizar resumen
+
+
+
+###############################
+######### Analizar de VIF #####
+###############################
+
+
+#### Problema
+
+####Suponga y utilidad de empresa
+#### suponga x gasto en publicidad
+#### Suponga x2 gasto en calidad
+
+###### se quiere comprobar si aumentar gasto en publicidad mejora utilidades
+
+
+###### Ajuste un modelo con sólo el gasto en calidad
+###### Cuánto crece la utilidad si se aumento una unidad la inversion en calidad
+
+
+
+### Agregue a ese modelo la variable publicidad
+### ¿Qué pasa con la relación entre utilidad y calidad?
+
+
+### Verifique la multicolinealidad con funcion vif
+
+
+
+
+
+
+
+
+
+
+
