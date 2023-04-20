604bfe7804f929b77694f38dbd6e5a42a12c01fb
juancamiloespana <camilo1260@gmail.com>
2023-04-20 16:14:14 -0500

    regresión lineal completo

--- 009_RLM_ejercicio_completo.R
+++ 009_RLM_ejercicio_completo.R
@@ -1,55 +1,102 @@
-
-
+#install.packages('caTools')
+#install.packages('plotly')
+#install.packages('regclass')
+#install.packages('skimr')
 #####Cargar librerías, las librerías siempre se deberían cargar al principio 
 
-  library(corrplot) ###Gráfico de correlaciones
-  library(plotly) ###para gráficos bonitos
-  library(Metrics) ### para calcular mape
-  library(regclass) ### para VIF
-  library(car)  ### validacion supuestos
-  library(MASS) ### para stepAIC
-  library(tseries) ### validacion supuestos
-  library(nortest) ### validacion supuestos
-  library(goftest) ### validacion supuestos
-  library(lmtest) ### validacion supuestos
-  library(randomForest) #### para imputación de datos faltantes
-  library(dplyr) #### para case_when para reagrupar variables
-  library(skimr) ### para descripcion de los datos 
+library(corrplot) ###Gráfico de correlaciones
+library(plotly) ###para gráficos bonitos
+library(Metrics) ### para calcular mape
+library(regclass) ### para VIF
+library(car)  ### validacion supuestos
+library(MASS) ### para stepAIC
+library(tseries) ### validacion supuestos
+library(nortest) ### validacion supuestos
+library(goftest) ### validacion supuestos
+library(lmtest) ### validacion supuestos
+library(randomForest) #### para imputación de datos faltantes
+library(dplyr) #### para case_when para reagrupar variables
+library(skimr) ### para descripcion de los datos 
+library(caTools) ###separar muestra entrenamiento y evaluacion
 
 
 #######Cargar Base de Datos-ejemplo ########3
-  
 
-
+ruta='https://raw.githubusercontent.com/juancamiloespana/DEAR_JCE/master/data/base_supermercado2.csv'
+df= read.csv(ruta,stringsAsFactors = T)
 ##########################################################################################################
 #### 1. Descripción y limpieza de los datos #############################################################
 #########################################################################################################
 
 
 ### Describir datos y analizar los tipos de variables 
-### Revisar que las variables queden en el formato correcto 
+skim(df)
+summary(df)
+
+
 ### Exploración inicial, verificar faltantes, números de categorías
+table(is.na(df)) ##calcula los datos faltantes en todas la tabla
+df2=na.roughfix(df) ##imputa mediana a numericas y moda a categóricas
+table(is.na(df2))### verificar si eliminaron faltantes
 
-#### Tratar datos faltantes 
+### revisar categorías en variables categóricas:
+
+table(df2$genero) ## está bien pocas cat y muchas observaciones en cada cat
+table(df2$reportado_data_credito)
+
+df2$reportado_data_credito=case_when(
+  df2$reportado_data_credito=='SI' ~ 'SI',
+  TRUE~'NO'
+  
+)
+df2$reportado_data_credito=as.factor(df2$reportado_data_credito)
+
+table(df2$medio_apgo)
+
+df2$medio_apgo=case_when(
+  df2$medio_apgo=='TD' ~ 'TD',
+  df2$medio_apgo=='TDC' ~ 'TDC',
+  TRUE~'Efectivo'
+  
+)
+
+df2$medio_apgo=as.factor(df2$medio_apgo)
 
 
-### revisar categorías en variables categóricas 
+sorted_count(df2$producto_frecuente)
+
+df2$producto_frecuente=case_when(
+  df2$producto_frecuente=='Pollo' ~ 'Pollo',
+  df2$producto_frecuente=='Carne' ~ 'Carne',
+  TRUE~'Otro'
+  
+)
+
+df2$producto_frecuente=as.factor(df2$producto_frecuente)
+
 
 #### Una variable con muchas categorías es difícil de analizar, puede hacer el modelo lento y 
 #### generar sobre ajustes, aunque no hay un número exacto para saber qué son muchas, podría utilizarse más de 20 o 30 como referencia
 #### según el estudio este valor se puede aumentar o disminuir
 #### Además se debe revisar que ninguna categoría tenga pocas observaciones
+
+
 ####Tratamiento de fechas 
+df2$fecha_ultima_compra2=as.Date(df2$fecha_ultima_compra, format="%d/%m/%Y")
+df2=df2[,-20] ### para eliminar columna de fecha repetida
+
+df2$m_fuc=months.Date(df2$fecha_ultima_compra2) ##extrae el mes de una fecha
+
 ##se convierte la columna a formato fecha
 ###los formatos se pueden encontrar en la ayuda de la función strptime
-  
+
 ### para extraer el mes
 ###para extraer el día de la semana
 ### Para extraer el trimestre
 ###Para extraer el día del mes
 
-  
-  
+
+
 ###############################################################################################
 ## 2. Análisis exploratorio #####################################################################
 ################################################################################################
@@ -57,7 +104,7 @@
 
 #### Explorar variable respuesta 
 ####Histograma sencillo 
-  
+
 ####Histograma fancy
 
 #### separar variables categóricas y numéricas
@@ -78,8 +125,8 @@
 #### verificar la colinealidad de variables a eliminar con VIF
 ####  seleccionar variables
 #### validar interacciones 
-  
-  
+
+
 
 ############################################################################################
 #### 4. Análisis de supuestos ##############################################################
@@ -91,6 +138,5 @@
 #### 5. Inferencias y análisis #############################################################
 ############################################################################################
 
-  
- 
- 
+
+
